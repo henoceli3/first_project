066f883c16ead35d64323e0e5f0de01bc56b6a7d
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2023-10-26 15:47:52 +0000

    2

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const port = process.env.PORT || 3000;
+  await app
+    .listen(port)
+    .then(() => {
+      console.log(`Application is running on: http://localhost:${port}`);
+    })
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
 }
 bootstrap();
